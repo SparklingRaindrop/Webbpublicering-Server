ba8f4022f79005ff91d1688a700617a69357b22e
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-08-22 15:51:00 +0200

    Init knex

---
+++ knex/migrations/20220822130518_first_migration.js
@@ -0,0 +1,56 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function up(knex) {
+    const userTable = knex.schema.hasTable('User').then(function(exists) {
+        if (!exists) {
+            return knex.schema.createTable('User', function(table) {
+                table.string('id').primary();
+                table.string('name').notNullable();
+                table.string('current_room_id').notNullable();
+                table.foreign('current_room_id').references('Room.id');
+            })
+        }
+    });
+
+    const roomTable = knex.schema.hasTable('Room')
+        .then(function(exists) {
+            if (!exists) {
+                return knex.schema.createTable('Room', function(table) {
+                    table.increments('id').primary();
+                    table.string('name').notNullable().unique();
+                    table.string('password');
+                })
+            }
+        });
+
+    const  messageTable = knex.schema.hasTable('Message').then(function(exists) {
+        if (!exists) {
+            return knex.schema.createTable('Message', function(table) {
+                table.increments('id').primary();
+                table.text('content').notNullable();
+                table.string('sender').notNullable();
+                table.string('sender_name').notNullable();
+                table.string('receiver');
+                table.integer('room_id');
+                table.timestamp('timestamp').defaultTo(knex.fn.now());
+                table.foreign('room_id').references('Room.id').onDelete('CASCADE');
+            })
+        }
+    });
+
+    return Promise.all([userTable, roomTable, messageTable]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return Promise.all([
+        knex.schema.dropTable('User'),
+        knex.schema.dropTable('Room'),
+        knex.schema.dropTable('Message')
+    ]);
+};
---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: __dirname + '/knex/migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: __dirname + '/knex/migrations'
+    }
+  }
+
+};
